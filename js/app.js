f94806dce30aff91c2b007368ff20a12d46f781a
Kathy Toufighi <kathy.toufighi@gmail.com>
2019-03-21 20:02:10 -0400

    adding first components

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const App = () => ( < div >
+  < div className = "min-vh-100 ph4 flex flex-column" > { /* our navigation component */ } { /* our intro text component */ } <
+  /div> < div className = "flex flex-wrap container" > { / * our attractions list component *
+  / } < /
+  div > < /div>
+)
+
+ReactDOM.render( < App / > , document.getElementById('root'))
